defabc99d6fa8216a0ad65a57987882f01d3b133
yuskov <yuskov@gmail.com>
2017-04-24 18:07:30 -0700

    .

---
+++ PhoGalPortal/PhoGalPortal/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace PhoGalPortal.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
